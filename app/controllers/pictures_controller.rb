92f453ead598058334791471a02c63c652eb85ca
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-06-05 18:21:20 +0900

    a

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,35 @@
 class PicturesController < ApplicationController
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(create_params)
+    if @picture.save
+      redirect_to :root
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(create_params)
+      redirect_to :root
+    else
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to :root
+  end
+
+  private
+  def create_params
+    params.require(:picture).permit(:image, :comment).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  resources :pictures
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # devise_for :users
   root 'top#index'
